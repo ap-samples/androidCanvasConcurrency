7f2c6a1f1a50c397102314164ac392d3b8909970
Nordvind <artur.peka@gmail.com>
2013-06-11 22:55:40 +0300

    Copying permission statements added

--- src/eu/arthur/canvasconcurrencytest/BlueRect.java
+++ src/eu/arthur/canvasconcurrencytest/BlueRect.java
@@ -1,3 +1,17 @@
+/*  Copyright 2013 Arthur Peka
+ * 
+ *  This file is part of CanvasConcurrencyTest.
+    CanvasConcurrencyTest is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.   
+    CanvasConcurrencyTest is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CanvasConcurrencyTest.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package eu.arthur.canvasconcurrencytest;
 
 import android.graphics.Canvas;
